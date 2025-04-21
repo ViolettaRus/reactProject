9ad907829dcd38a9b806557a8bbfd36e857e60f7
violetta <violetta.rusovich.2002@gmail.com>
2025-04-21 17:25:15 +0300

    TASK-003

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  text-align: center;
+`
+
+function App() {
+  return (
+    <Div>
+      <i className="fa fa-camera-retro"></i>
+      <div>234</div>
+    </Div>
+  );
+}
+
+export default App;
